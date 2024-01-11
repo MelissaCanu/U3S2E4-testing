652b013f6073c52fa191d682f244957c41c93ab2
MelissaCanu <melissamcanu@gmail.com>
2024-01-11 16:53:26 +0100

    problem with filter test, books.filter

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,10 +1,11 @@
 /* creato cartella __tests__ >>> con file App.test.jsx */
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Welcome from "../components/Welcome";
 import AllTheBooks from "../components/AllTheBooks";
 import fantasy from "../data/fantasy.json";
-import CommentArea from "../components/CommentArea";
-import { waitFor } from "@testing-library/react";
+// import CommentArea from "../components/CommentArea";
+// import { waitFor } from "@testing-library/react";
+import BookList from "../components/BookList";
 
 /* Welcome Component Rendering */
 
@@ -35,12 +36,28 @@ describe("All book-cards Test", () => {
 
 /*CommentArea Rendering*/
 
-describe("CommentArea Test", () => {
-	it("Commentarea renders correctly", async () => {
-		render(<CommentArea />);
-		await waitFor(() => {
-			const loadingComponents = screen.getAllByTestId("loading-spinner");
-			expect(loadingComponents).toHaveLength(1); // Assumi che ci sia solo un elemento con data-testid="loading-spinner"
-		});
+// describe("CommentArea Test", () => {
+// 	it("Commentarea renders correctly", async () => {
+// 		render(<CommentArea />);
+// 		await waitFor(() => {
+// 			const loadingComponents = screen.getAllByTestId("loading-spinner");
+// 			expect(loadingComponents).toHaveLength(1); // Assumi che ci sia solo un elemento con data-testid="loading-spinner"
+// 		});
+// 	});
+// });
+
+/* fireEvent >>>>>>>> fireEvent.change prende due argomenti: 
+l'elemento target e un oggetto che rappresenta l'evento. <<<<<<<< */
+
+/*  Filter test */
+
+describe("Filters correctly", () => {
+	it("Finds two results for 'long'", () => {
+		render(<BookList />);
+		const filterFormControl = screen.getByPlaceholderText(/cerca un libro/i);
+		fireEvent.change(filterFormControl, { target: { value: "long" } });
+		/* qua riciclo da sopra */
+		const allTheBookCards = screen.getAllByTestId("book-card");
+		expect(allTheBookCards).toHaveLength(2);
 	});
 });
